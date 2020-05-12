a48045b34615d1a8c41b8e090f54aa235f9fe08e
dorjepadma <dorjepadma@gmail.com>
2020-05-12 15:27:42 -0700

    moodyActions done

--- src/actions/moodyActions.js
+++ src/actions/moodyActions.js
@@ -1,23 +1,17 @@
-handleSelection = action => {
-  switch(action.type) {
-    case 'DRINK_COFFEE':
-      this.setState(state => ({ coffees: state.coffees + 1 }));
-      break;
-    case 'EAT_SNACK':
-      this.setState(state => ({ snacks: state.snacks + 1 }));
-      break;
-    case 'TAKE_NAP':
-      this.setState(state => ({ naps: state.naps + 1 }));
-      break;
-    case 'STUDY':
-      this.setState(state => ({ studies: state.studies + 1 }));
-      break;
-    default:
-      console.log(`unhandled type: ${action.type}`);
-  }
-}
 export const DRINK_COFFEE = 'DRINK_COFFEE';
 export const drinkCoffee = () => ({
   type: DRINK_COFFEE
+});
+export const EAT_SNACK = 'EAT_SNACK';
+export const eatSnack = () => ({
+  type: EAT_SNACK
+});
+export const TAKE_NAP = 'TAKE_NAP';
+export const takeNap = () => ({
+  type: TAKE_NAP
+});
+export const STUDY = 'STUDY';
+export const study = () => ({
+  type: STUDY
 });
 
